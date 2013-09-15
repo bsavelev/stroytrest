90702edb84a2e3b5908d8370e2f24ed41908d33e
Alexandrov-Michael <mikle.alex@gmail.com>
2013-09-15 13:42:22 +0400

    settings

--- project/local_settings.py
+++ project/local_settings.py
@@ -6,11 +6,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stroytrest1',                      # Or path to database file if using sqlite3.
+        'NAME': 'stroytrest_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'postgres',
-        'PASSWORD': '0',
-        'HOST': '192.166.1.20',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
